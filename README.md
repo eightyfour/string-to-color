5340412f7dba09cfe08f656351dd1cb8c79ba7c3
eightyfour <eightyfour@thinkuseful.de>
2016-12-04 20:23:50 +0100

    add doc - create pub api

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# stringToColor
+# string-to-color
 Generates a color code from a string - supports rgba and hex format
+
+### convert string to HEX
+```js
+stringToColor.convertHex('convertSampleText') // > "#0c15cd"
+```
+
+### convert string to RGBA
+```js
+stringToColor.convertRGBA('convertSampleText',.3) // > "rgba(12,21,205,0.3)"
+```
--- stringToColor.js
+++ stringToColor.js
@@ -15,22 +15,43 @@ function convertToHex(str) {
     var b = hash & 0x0000FF;
     return "#" + ("0" + r.toString(16)).substr(-2) + ("0" + g.toString(16)).substr(-2) + ("0" + b.toString(16)).substr(-2);
 }
-
+/**
+ *
+ * @param hex
+ * @param opacity
+ * @returns {string}
+ */
 function convertHexToRGBA(hex, opacity) {
     var hexNoHash = hex.replace('#', ''),
         r = parseInt(hexNoHash.substring(0, 2), 16),
         g = parseInt(hexNoHash.substring(2, 4), 16),
         b = parseInt(hexNoHash.substring(4, 6), 16),
-        result = 'rgba(' + r + ',' + g + ',' + b + ',' + opacity / 100 + ')';
+        result = 'rgba(' + r + ',' + g + ',' + b + ',' + opacity || 1 + ')';
 
     return result;
 }
 
 module.exports = {
-    convertHex : function (str) {
+    /**
+     * convert a string to a hex color code
+     * @param str
+     */
+    toHex : function (str) {
         return convertToHex(str);
     },
-    convertRGBA : function (str, opacity) {
+    /**
+     * Convert a string to a rgba color
+     * @param str
+     * @param opacity [opacity=1]
+     */
+    toRGBA : function (str, opacity) {
         return convertHexToRGBA(convertToHex(str), opacity)
-    }
+    },
+    /**
+     * Convert a HEX to rgba. E.g. #330000 > rgba(51,0,0,1)"
+     * @param hex
+     * @param opacity [opacity=1]
+     * @returns {string}
+     */
+    convertHexToRGBA : convertHexToRGBA
 };
